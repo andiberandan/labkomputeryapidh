9303983d30f71e2d0b97d53da89ac7adfcfab10a
andiberandan <wilfadh@gmail.com>
2024-10-22 10:19:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+  // Fungsi ini akan dipanggil ketika tombol diklik
+  function showMessage() {
+    alert("Tombol telah diklik!");
+  }
+
+  // Fungsi ini akan mengubah teks di dalam elemen HTML
+  function changeText() {
+    document.getElementById("text").innerHTML = "Teks telah diubah!";
+  }
+</script>
